c435690e48017d8519f6892d7e74f273b493b104
Marwan Ali <84754292+MarwanAliii@users.noreply.github.com>
2025-05-01 21:09:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -74,3 +74,6 @@ def push_to_github(content):
     put_resp = requests.put(url, headers=headers, json=data)
     put_resp.raise_for_status()
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
